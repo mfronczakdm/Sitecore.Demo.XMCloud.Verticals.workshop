a6f34b1f1494298dbecc3ae6b2f60db0bf697b14
mfronczakdm <mfronczak1@gmail.com>
2024-07-11 15:49:24 -0400

    update promo cta

--- src/sxastarter/src/components/PageContent/PromoCta.tsx
+++ src/sxastarter/src/components/PageContent/PromoCta.tsx
@@ -40,7 +40,7 @@ export const Default = (props: PromoCtaProps): JSX.Element => {
       event: 'Button Clicked',
       properties: {
         Page: route.name,
-        Button: props.params.name,
+        Button: route.name,
       },
     });
   }
